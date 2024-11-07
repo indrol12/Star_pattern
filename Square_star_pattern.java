d6e1c374b6ee549af659b9959c13596c60cc096c
Suyash Sonawane <suyashgit24@gmail.com>
2024-11-07 15:16:43 +0530

    Create Square_star_pattern.java

---
+++ Square_star_pattern.java
@@ -0,0 +1,19 @@
+package Day_five;
+
+public class Solid_square_star_pattern 
+{
+
+	public static void main(String[] args) 
+	{
+
+		for(int r=1;r<=5;r++)
+		{
+			for(int c=1;c<=5;c++)
+			{
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
+
+}

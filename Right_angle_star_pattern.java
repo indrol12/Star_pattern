ee462117397d3e5a183c5dc34ee398468c3f2d54
Suyash Sonawane <suyashgit24@gmail.com>
2024-11-07 15:17:51 +0530

    Create Right_angle_star_pattern.java

---
+++ Right_angle_star_pattern.java
@@ -0,0 +1,19 @@
+package Day_five;
+
+public class Right_angle_star_pattern 
+{
+
+	public static void main(String[] args) 
+	{
+
+		for(int r=1;r<=5;r++)
+		{
+			for(int c=1;c<=r;c++)
+			{
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
+
+}
